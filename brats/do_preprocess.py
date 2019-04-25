d42dd14b6cebb3d83e1567538294a6e29c835f8c
aalinc9 <chingheng113@gmail.com>
2019-04-25 15:02:40 -0400

    back up

--- brats/do_preprocess.py
+++ brats/do_preprocess.py
@@ -1,2 +1,4 @@
+import os, sys
+sys.path.append(os.path.abspath(os.path.join('/data/linc9/3DUnetCNN/')))
 from brats.preprocess import convert_brats_data
 convert_brats_data("data/original", "data/preprocessed")
